2831832b39f2ab084fa9cc86a59e5bafbbd13a9a
algididntwakeup <algisosmedacc@gmail.com>
2024-10-14 17:07:02 +0700

    Controller

---
+++ Sesi4/app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+class About
+{
+    public function index($nama = 'Algi', $pekerjaan = 'Estutiante')
+    {
+        echo "Hola, soy $nama. soy $pekerjaan.";
+    }
+    public function page()
+    {
+        echo "About/page";
+    }
+}
